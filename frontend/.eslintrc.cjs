962224db709c10d3b9b97dbabc274ac4bc6692b5
christoph-teichmeister <christoph.teichmeister@gmail.com>
2025-12-26 01:37:00 +0100

    chore(frontend): integrate `@typescript-eslint/eslint-plugin`, update ESLint configuration for TypeScript support, and adjust dependencies

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -4,12 +4,29 @@ module.exports = {
     browser: true,
     es2024: true
   },
-  extends: ['eslint:recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module'
   },
-  plugins: ['svelte'],
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['svelte', '@typescript-eslint'],
   overrides: [
+    {
+      files: ['*.ts', '**/*.ts'],
+      excludedFiles: ['*.d.ts', '**/*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json'
+      }
+    },
+    {
+      files: ['*.d.ts', '**/*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'off'
+      }
+    },
     {
       files: ['*.svelte', '**/*.svelte'],
       parser: 'svelte-eslint-parser',
@@ -22,6 +39,15 @@ module.exports = {
       rules: {}
     }
   ],
-  rules: {},
-  ignorePatterns: ['.svelte-kit', 'node_modules']
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_'
+      }
+    ]
+  },
+  ignorePatterns: ['.svelte-kit', 'node_modules', 'node_modules.orig', 'svelte.config.js']
 };
